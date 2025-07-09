e16e835de8e129c97e582656826128312d477c0f
Thomas Rosen <thomas.rosen@brandung.de>
2025-07-09 12:08:23 +0200

    replaced eslint with biome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,24 @@
-import { Toaster } from '@/components/ui/sonner'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Toaster } from '@/components/ui/sonner';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Error Codes',
   description: '',
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>{children}</body>
       <Toaster />
     </html>
-  )
+  );
 }
